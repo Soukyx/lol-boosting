1c511825f494b682e29f9a7a01ebd4568c5f51a1
Soukyx <jena00004@seznam.cz>
2021-11-16 09:33:02 +0100

    přidání dalších stránek + doladění registrace

--- connect.php
+++ connect.php
@@ -14,7 +14,7 @@
 		$stmt->bind_param("sss", $name, $email, $password);
 		$execval = $stmt->execute();
 		echo $execval;
-		echo "Registrace proběhla úspěšně";
+		header('Location: login.html');
 		$stmt->close();
 		$conn->close();
 	}
