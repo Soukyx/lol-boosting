a45e9b07ba3c2f1972281715417c817dc5c4c42e
Soukyx <jena00004@seznam.cz>
2021-11-23 08:43:29 +0100

    komenty

--- php/connect.php
+++ php/connect.php
@@ -6,9 +6,11 @@
 
 	// připojení na databázi
 	$conn = new mysqli('localhost','root','','user');
+	//pokud se nepřipojí hodí error
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
+	//pokud se připojí vypíše do databáze poté nás hodí na stránku loginu
 	} else {
 		$stmt = $conn->prepare("insert into registration(name, email, password) values(?, ?, ?)");
 		$stmt->bind_param("sss", $name, $email, $password);
