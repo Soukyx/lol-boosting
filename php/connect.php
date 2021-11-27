08e7ff77ad369147fbcc9b7c8bd10cced25b29a2
Soukyx <jena00004@seznam.cz>
2021-11-27 14:42:41 +0100

    register

--- php/connect.php
+++ php/connect.php
@@ -6,27 +6,19 @@
     $cpassword = $_POST['cpassword'];
 	// připojení na databázi
 	$conn = new mysqli('localhost','root','','user');
-	//pokud se nepřipojí hodí error
 	if($conn->connect_error){
 		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);}
-	//pokud se připojí vypíše do databáze poté nás hodí na stránku loginu
-		//pokud zadaný email existuje registrace neproběhne (nefunguje:D)
-//	elseif(mysql_num_rows($selectresult)>0)
-  //  {
-    //     $msg = 'email already exists';
-    //}
-	//pokud se hesla neshodují registrace neproběhne
-    else if($password != $cpassword){
-         $msg = "passwords doesn't match";
-    }
-	else{
+		die("Connection Failed : ". $conn->connect_error);
+	} 
+	else if($password != $cpassword){
+		$msg = "passwords doesn't match";
+   }else {
 		$stmt = $conn->prepare("insert into registration(name, email, password, cpassword) values(?, ?, ?, ?)");
 		$stmt->bind_param("ssss", $name, $email, $password, $cpassword);
 		$execval = $stmt->execute();
 		echo $execval;
+		echo "Registration successfully...";
 		$stmt->close();
 		$conn->close();
 	}
-	
 ?>
